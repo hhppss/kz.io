8879dabc2631fabe8989a0ea4239d6e0f1184d99
hhppss <46415976+hhppss@users.noreply.github.com>
2020-04-11 21:52:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.header-burger').click(function(event) {
+    $('.header-burger, .header-menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  })
+  $('.header-link').click(function(event) {
+    $('.header-burger, .header-menu').removeClass('active');
+    $('body').removeClass('lock');
+  })
+  $('.shop-item__visible').click(function(event) {
+    $('.closes').toggleClass('closeup');
+  })
+});
